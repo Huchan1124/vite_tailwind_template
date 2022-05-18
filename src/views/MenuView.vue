cefc7cf7dd3838cbdf91990d8858b7820f8c6402
alicia <garlic1124@gmail.com>
2022-05-18 15:56:45 +0800

    add grid style

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -12,14 +12,37 @@
       </h1>
     </div>
 
-    <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 ">
-      <div class=" bg-blue-500  ">01</div>
-      <div class=" bg-blue-500 ">02</div>
+    <div class="grid grid-rows-3 grid-cols-3 grid-flow-col  gap-4 p-4">
+      <div class=" bg-blue-500  row-span-1">01</div>
+      <div class=" bg-blue-500 col-span-2">02</div>
       <div class=" bg-blue-500 ">03</div>
       <div class=" bg-brown-500">04</div>
       <div class=" text-brown-600 wavy-underline">055555555555555555555</div>
+      <div class="row-span-2">
+        <img class=" max-w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1597633425046-08f5110420b5?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470"
+          alt="柯基圖片">
+      </div>
+      <div>
+        <img class="max-w-full h-auto"
+          src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469"
+          alt="柯基圖片2">
+      </div>
+    </div>
+    <div class="p-4 text-justify">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eum eius enim animi! Consequatur ipsum iusto
+        nulla excepturi vero magni repudiandae numquam, animi, labore libero quia velit aut placeat ratione, quis vel.
+        Iusto iure cupiditate beatae sapiente ullam. Aliquam, distinctio animi! Omnis dignissimos molestias quibusdam
+        sint veritatis adipisci harum sapiente nam officiis cupiditate voluptate non fugit quisquam dicta minima
+        explicabo voluptatum maxime alias provident sunt, ducimus praesentium optio deleniti? Quidem, dolore adipisci
+        doloremque cumque unde qui, veritatis odit ipsa assumenda officia, tempora quas sed ipsum sit magnam itaque
+        ipsam illo odio ut architecto? Laborum quia dolorem maxime. Quos, culpa et.</p>
     </div>
   </div>
+
+
+
+
 
 
 
