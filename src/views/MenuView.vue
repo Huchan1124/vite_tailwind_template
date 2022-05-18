3ee335452ab285cea8972e423a9564250ca3763e
alicia <garlic1124@gmail.com>
2022-05-18 11:55:10 +0800

    first commit

---
+++ src/views/MenuView.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="container mx-auto ">
+    <header class="bg-slate-100 p-4 text-center">
+      <h1 class="text-3xl text-rose-500 font-bold underline ">
+        Hello world!
+      </h1>
+    </header>
+
+    <div class="grid gap-4 grid-cols-1 md:grid-cols-3 p-4 ">
+      <div class=" bg-blue-500  ">01</div>
+      <div class=" bg-blue-500 ">02</div>
+      <div class=" bg-blue-500 ">03</div>
+      <div class=" bg-brown-500">04</div>
+      <div class=" text-brown-600 ">05</div>
+    </div>
+  </div>
+
+
+
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+})
